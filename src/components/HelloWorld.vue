8112dfb300b9adcbcebe06156268f6a7fea64dca
yk952 <2754712272@qq.com>
2025-08-03 01:35:18 +0800

    Learn TypeScript basic types: string, number, array

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
-<<<<<<< HEAD
+
 import { username, age, add } from '../types/basic-types';
 import { ref } from 'vue';
 
 defineProps<{ msg: string }>();
 
 const count = ref(0);
-=======
-import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
->>>>>>> 193c745eb46044370b3337ad0e0a0d1b3ace5be1
 </script>
 
 <template>
@@ -25,16 +19,12 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-<<<<<<< HEAD
   <div>
     <p>Name: {{ username }}</p>
     <p>Age: {{ age }}</p>
     <p>1 + 2 = {{ add(1, 2) }}</p>
   </div>
-  
-=======
 
->>>>>>> 193c745eb46044370b3337ad0e0a0d1b3ace5be1
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
@@ -50,10 +40,7 @@ const count = ref(0)
     >.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-<<<<<<< HEAD
-  
-=======
->>>>>>> 193c745eb46044370b3337ad0e0a0d1b3ace5be1
+
 </template>
 
 <style scoped>
