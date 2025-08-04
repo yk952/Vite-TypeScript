409bad4002c3658a45731d696fb4591658ac3398
yk952 <2754712272@qq.com>
2025-08-04 14:17:41 +0800

    Add Vite environment variables

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// 使用环境变量设置标题
+document.title = import.meta.env.VITE_APP_TITLE || "My App"
+app.mount('#app')
