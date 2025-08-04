c7e68845a048e78f80d6d41e6f5277d5e9cca256
yk952 <2754712272@qq.com>
2025-08-04 14:33:33 +0800

    Install Arco Design and Vue Router

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ArcoVue from '@arco-design/web-vue' 
+import '@arco-design/web-vue/dist/arco.css' 
+import router from './router' 
 
 const app = createApp(App)
 // 使用环境变量设置标题
 document.title = import.meta.env.VITE_APP_TITLE || "My App"
+app.use(ArcoVue) 
+app.use(router)
 app.mount('#app')
